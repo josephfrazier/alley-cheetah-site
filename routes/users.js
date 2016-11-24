16aed019a102a42c03a79a92f68dd37e89ccad95
Joseph Frazier <1212jtraceur@gmail.com>
2016-11-24 18:05:06 -0500

    get proof-of-concept working
    Note that it needs a Google Maps API Key:
    
        GOOGLE_MAPS_API_KEY=YOUR_API_KEY DEBUG=server:* npm start

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var alleyCheetah = require('alley-cheetah');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  var {origin, destination} = req.body;
+  var waypointGrid = [
+    req.body.row1,
+    req.body.row2,
+    req.body.row3,
+    req.body.row4,
+    req.body.row5,
+  ]
+
+  // remove empty cells
+  waypointGrid = waypointGrid.map(row => row.filter(cell => cell.length));
+  waypointGrid = waypointGrid.filter(row => row.length);
+
+  alleyCheetah({origin, destination, waypointGrid}).then(function ({route, waypoints}) {
+    var link = alleyCheetah.getMapsLink({origin, destination, waypoints})
+    res.send(`<a href=${link}>${link}</a>`);
+  });
 });
 
 module.exports = router;
