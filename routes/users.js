9ba36d4f65af73508dc51956eaa271cc5d9cd761
Joseph Frazier <1212jtraceur@gmail.com>
2016-11-25 13:54:44 -0500

    Pass memoization function to alley-cheetah
    See https://github.com/josephfrazier/alley-cheetah/commit/d6fe027ee2339d90fb667654e02a86f4f6b2f75e

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var alleyCheetah = require('alley-cheetah');
+var cachePath = require('path').join(__dirname, '..', 'cache')
+var memoize = require('memoize-fs')({ cachePath: cachePath })
+
+var memoizeFn = memoize.fn
 
 router.post('/', function(req, res, next) {
   var {origin, destination} = req.body;
@@ -16,7 +20,7 @@ router.post('/', function(req, res, next) {
   waypointGrid = waypointGrid.map(row => row.filter(cell => cell.length));
   waypointGrid = waypointGrid.filter(row => row.length);
 
-  alleyCheetah({origin, destination, waypointGrid}).then(function ({route, waypoints}) {
+  alleyCheetah({origin, destination, waypointGrid, memoizeFn}).then(function ({route, waypoints}) {
     var link = alleyCheetah.getMapsLink({origin, destination, waypoints})
     res.send(`<a href=${link}>${link}</a>`);
   }).catch(next)
