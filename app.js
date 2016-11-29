2ed4c0947c7aaf7281a9b8a9a0e624136e042671
Joseph Frazier <1212jtraceur@gmail.com>
2016-11-28 20:47:12 -0500

    app.js: remove stale comment

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
